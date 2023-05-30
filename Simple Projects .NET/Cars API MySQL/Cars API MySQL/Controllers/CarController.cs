e26b501977916d8cf6496f390822bede7f5235b2
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-05-29 19:36:29 -0500

    Adición controlador autos (01. Cars API)

---
+++ Simple Projects .NET/Cars API MySQL/Cars API MySQL/Controllers/CarController.cs
@@ -0,0 +1,63 @@
+﻿using Cars_API_MySQL.Data.Repositories;
+using Cars_API_MySQL.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cars_API_MySQL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly ICarRepository _carRepository;
+
+        public CarController(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCars()
+        {
+            return Ok(await _carRepository.GetAllCars());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCarDetails(int id)
+        {
+            return Ok(await _carRepository.GetDetails(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCar([FromBody] Car car)
+        {
+            if (car == null) return BadRequest();
+
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var created = await _carRepository.InsertCar(car);
+
+            return Created("created", created);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCar([FromBody] Car car)
+        {
+            if (car == null) return BadRequest();
+
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            await _carRepository.UpdateCar(car);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCar(int id)
+        {
+            await _carRepository.DeleteCar(new Car { Id = id });
+
+            return NoContent();
+        }
+    }
+}
