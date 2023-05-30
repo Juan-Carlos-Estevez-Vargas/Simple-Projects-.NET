9aff99de669de8dd3c69707500247fbcca917361
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-05-29 19:23:55 -0500

    Adición repositorios para los autos (01. Cars API)

---
+++ Simple Projects .NET/Cars API MySQL/Cars API MySQL.Data/Repositories/CarRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars_API_MySQL.Data.Repositories
+{
+    internal class CarRepository
+    {
+    }
+}
---
+++ Simple Projects .NET/Cars API MySQL/Cars API MySQL.Data/Repositories/ICarRepository.cs
@@ -0,0 +1,18 @@
+﻿using Cars_API_MySQL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars_API_MySQL.Data.Repositories
+{
+    public interface ICarRepository
+    {
+        Task<IEnumerable<Car>> GetAllCars();
+        Task<Car> GetDetails(int id);
+        Task<bool> InsertCar(Car car);
+        Task<bool> UpdateCar(Car car); 
+        Task<bool> DeleteCar(Car car);
+    }
+}
