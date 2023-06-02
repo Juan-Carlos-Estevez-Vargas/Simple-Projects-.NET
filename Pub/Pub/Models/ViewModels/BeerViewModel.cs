c5c3d6c6aac3569b1f4983a92a47d720f6fab1e0
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-06-02 18:33:47 -0500

    ViewModels y Validaciones

--- Pub/Pub/Models/ViewModels/BeerViewModel.cs
+++ Pub/Pub/Models/ViewModels/BeerViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace Pub.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pub.Models.ViewModels
 {
     public class BeerViewModel
     {
+        [Required]
+        [Display(Name = "Nombre")]
+        public int BrandId { get; set; }
+
+        [Required]
+        [Display(Name = "Marca")]
+        public string Name { get; set; }
     }
 }
